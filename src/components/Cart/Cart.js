bd2c5b69c7362744e39d13fe0c686c34367ec8e3
Divyam Kakkar <kakkardivyam24@gmail.com>
2022-07-10 17:16:41 +0530

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,7 +12,7 @@ const Cart = props => {
 	const [didSubmit, setDidSubmit] = useState(false);
 	const cartCtx = useContext(CartContext);
 
-	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
 	const cartItemRemoveHandler = id => {
